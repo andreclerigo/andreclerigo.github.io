611259bc5d97bc884ba935277a2d61024c0a730b
andreclerigo Desktop <andreclerigo@outlook.com>
2021-08-08 15:52:47 +0100

    changing favicon and navbar always on top layer

--- js/script.js
+++ js/script.js
@@ -4,11 +4,3 @@ $(function() {
         $('#sidebar, #content').toggleClass('active');
     });
 });
-
-// Smooth scrolling on navlink
-$('.nav-link, .navbar-brand, .new-button').click(function() {
-    var sectionTo = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top
-    }, 1500);
-});
